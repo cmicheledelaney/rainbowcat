97f398709de0b0c159a057e657565cba632ecf5a
cmicheledelaney <c.michele.delaney@gmail.com>
2019-03-15 10:48:41 -0700

    changes help file

--- includes/rainbowcat.h
+++ includes/rainbowcat.h
@@ -9,11 +9,12 @@
 #define WIDTH 127
 #define CENTER_PASTEL 200
 #define WIDTH_PASTEL 55
-#define FREQUENCY 0.3
+#define FREQUENCY 0.2
 #define PHASE1 0
 #define PHASE2 2
 #define PHASE3 4
 #define RESET "\033[0m"
+
 
 
 
