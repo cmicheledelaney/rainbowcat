a1a659ca63ca246cd2a3dc8640ac908e84300e30
Chantal Codiga <ccodiga@e1z3r5p26.42.us.org>
2019-03-14 19:22:26 -0700

    reads from stdin now

--- srcs/rainbowcat.c
+++ srcs/rainbowcat.c
@@ -2,22 +2,32 @@
 
 int		width, center;
 
+/*
+** produces strings with the 256 colorcodes available.
+*/
 char	**get_colorcodes(void)
 {
 	char	**colorcodes;
 	char	*code;
 	int		j, pos;
 
+	// this is the default color code. We only need to change the last
+	// three digits before 'm'.
 	code = "\033[01;38;5;000m";
 	colorcodes = (char **)malloc(sizeof(char *) * 256);
 	for (int i = 0; i < 256; i++)
 	{
 		colorcodes[i] = (char *)malloc(sizeof(char) * (strlen(code) + 1));
+		// copies the default colorcode
 		colorcodes[i] = strcpy(colorcodes[i], code);
 		j = i;
+		// sets the pos value to the last value before 'm'.
 		pos = strlen(code) - 2;
+		// the last three digits get changed so that they represent the
+		// string version of 'i'.
 		while (j > 0)
 		{
+			// the colorcode at pos gets set to the last digit of 'j'
 			colorcodes[i][pos] = (char)(j % 10) + '0';
 			pos--;
 			j /= 10;
@@ -26,6 +36,10 @@ char	**get_colorcodes(void)
 	return (colorcodes);
 }
 
+/*
+** produces a sin wave for each of the base colors (red, green, blue) and
+** combines them in a single value. That is done for each element of LEN.
+*/
 int *get_rainbow(void)
 {
 	int		*wave;
@@ -43,6 +57,11 @@ int *get_rainbow(void)
 	return (wave);
 }
 
+/*
+** reads the file line by line and prints it. color_codes contains all the
+** 256 colorcodes, sin_wave contains a sin wave of the rgb values that
+** produces a rainbow color transition.
+*/
 int		cat(FILE *file)
 {
 	char	*line;
@@ -52,16 +71,23 @@ int		cat(FILE *file)
 	int		i, up, reading;
 
 	line = NULL;
+	// gets the array of the 256 color codes
 	color_codes = get_colorcodes();
+	// gets the sin wave to acces the right colorcodes
 	sin_wave = get_rainbow();
 	i = 0;
 	up = 1;
+	// buff size
 	size = 10;
 	line = (char *)malloc(sizeof(char) * (size + 1));
+	// reads file line by line
 	while ((reading = getline(&line, &size, file)) != -1)
 	{
 		line[reading] = '\0';
+		// prints each line in the according color
 		printf("%s%s", color_codes[sin_wave[i]], line);
+		// checks if i has reached the end or the start of the array
+		// and moves in the opposite direction.
 		if (up == 1)
 		{
 			i++;
@@ -76,6 +102,10 @@ int		cat(FILE *file)
 	return (0);
 }
 
+/*
+** checks if the --pastel flag is on and sets the globals for
+** the center and the width to the pastel values instead.
+*/
 int		check_colorscheme(char *colorscheme)
 {
 	if (strcmp(colorscheme, "--pastel") == 0)
@@ -83,11 +113,6 @@ int		check_colorscheme(char *colorscheme)
 		width = WIDTH_PASTEL;
 		center = CENTER_PASTEL;
 		return (1);
-	}
-	else
-	{
-		width = WIDTH;
-		center = CENTER;
 	}
 	return (0);
 }
@@ -97,21 +122,39 @@ int		main(int argc, char **argv)
 	FILE	*file;
 
 	argv[argc] = NULL;
+	// sets center and width for the sine wave to the default, which produces
+	// brighter colors
+	center = CENTER;
+	width = WIDTH;
+	// reads from stdin if there are no arguments
+	if (argv[1] == NULL)
+		cat(stdin);
+	// iterates through the arguments
 	for (int i = 1; i < argc; i++)
 	{
+		// checks if the --pastel flag is on an sets the values accordingly
 		if (check_colorscheme(argv[i]))
 			i++;
+		// checks if there is an argument; if not, reads from standard input
 		if (argv[i] == NULL)
-			fprintf(stderr, "usage: ./rainbowcat [--pastel | --help | file/s]");
+		{
+			cat(stdin);
+			return (0);
+		}
+		// checks if the --help flag is on and sets the .help file as input
 		if (strcmp(argv[i], "--help") == 0)
 			argv[i] = ".help";
+		// opens the file
 		if ((file = fopen(argv[i], "r")) == NULL)
 		{
 			perror(argv[i]);
 			continue;
 		}
+		// prints the file
 		cat(file);
+		// if there are several files it prints a newline in between
 		argc > 2 && i < argc - 1 ? printf("\n") : 0;
+		// closes the filedescriptor
 		if (fclose(file) == -1)
 		{
 			perror(argv[i]);
